27bf7d8513abed9e93ffbad76b113313ac79c804
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2022-01-20 12:50:58 -0600

    Parallel-n64

---
+++ emulators/parallel-n64/romlist.js
@@ -0,0 +1,8 @@
+var ROMLIST = [
+    { url: "roms/Super_Mario 64_(USA).z64", title: "Super Mario 64 (USA)" }
+    /*
+    {url:"roms/rom1.z64",title:"Game 1"},
+    {url:"roms/rom2.v64",title:"Game 2"},
+    {url:"roms/rom3.v64",title:"Game 3"},
+    */
+];
---
+++ emulators/parallel-n64/settings.js
@@ -0,0 +1,5 @@
+var N64WASMSETTINGS = {
+    CLOUDSAVEURL: "",
+    SHOWADVANCED: true, //set this to true to enable importing of save files (eep, sra, fla)
+    SHOWFPS: true
+}

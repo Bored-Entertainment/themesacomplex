18c5c4db63ae3ee102cd3188189f4aecf6a05b6e
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2022-01-21 07:46:41 -0600

    Removed Needed Files

--- emulators/parallel-n64/romlist.js
+++ emulators/parallel-n64/romlist.js
@@ -1,5 +1,4 @@
 var ROMLIST = [
-    { url: "roms/Super_Mario 64_(USA).z64", title: "Super Mario 64 (USA)" }
     /*
     {url:"roms/rom1.z64",title:"Game 1"},
     {url:"roms/rom2.v64",title:"Game 2"},
